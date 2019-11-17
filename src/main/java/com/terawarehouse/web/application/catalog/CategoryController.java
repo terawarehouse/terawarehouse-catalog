d7d4d3dea4e43e460d9d4dd502aba91cd56e7a18
Edward P. Legaspi <czetsuya@gmail.com>
2019-11-17 17:23:58 +0800

    Override findAll for demo.

--- src/main/java/com/terawarehouse/web/application/catalog/CategoryController.java
+++ src/main/java/com/terawarehouse/web/application/catalog/CategoryController.java
@@ -23,14 +23,17 @@ import javax.transaction.NotSupportedException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.broodcamp.util.StringUtils;
@@ -55,5 +58,13 @@ public class CategoryController extends AbstractBusinessController<Category, Cat
         }
 
         return super.create(dto);
+    }
+
+    @Override
+    @GetMapping
+    public CollectionModel<EntityModel<CategoryDto>> findAll(@RequestParam(required = false) Integer size, @RequestParam(required = false) Integer page)
+            throws NotSupportedException {
+
+        return super.findAll(size, page);
     }
 }
